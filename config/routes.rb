2cb27cb6f9e5d8d674dc6254d8f9a54811c0781b
anhtn <tongngocanhcampha@gmail.com>
2021-12-05 15:04:28 +0700

    Add Home

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
